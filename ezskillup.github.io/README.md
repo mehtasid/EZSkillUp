a7655c134dc3832efa5a49ab950b196f82acb46d
mehtasid <69875947+mehtasid@users.noreply.github.com>
2022-02-12 17:04:03 -0800

    Add files via upload

---
+++ ezskillup.github.io/README.md
@@ -0,0 +1,1 @@
+# ezskillup.github.io
---
+++ ezskillup.github.io/skill_pages/test.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+
+    $('.button1').hover(function() {
+  
+      $('#text1').css('opacity', '1');
+    }, function() {
+      // on mouseout, reset the opacity
+      $('#text1').css('opacity', '0');
+  
+    });
+  
+    $('.button2').hover(function() {
+  
+      $('#text2').css('opacity', '1');
+    }, function() {
+      // on mouseout, reset the opacity
+      $('#text2').css('opacity', '0');
+  
+    });
+  
+    $('.button3').hover(function() {
+  
+      $('#text3').css('opacity', '1');
+    }, function() {
+      // on mouseout, reset the opacity
+      $('#text3').css('opacity', '0');
+  
+    });
+  
+    $('.button4').hover(function() {
+  
+      $('#text4').css('opacity', '1');
+    }, function() {
+      // on mouseout, reset the opacity
+      $('#text4').css('opacity', '0');
+  
+    });
+  
+  });
+  
